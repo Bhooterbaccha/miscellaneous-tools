e6d6d503d26b95844dc4f0b1b976624147158a3e
Avirup Mukherjee <Avimukh.250696@gmail.com>
2017-11-12 23:10:30 +0530

    Update dataset_len.py

--- dataset_len.py
+++ dataset_len.py
@@ -2,6 +2,7 @@ import sys
 inp_file = sys.argv[1]
 out_file = sys.argv[2]
 count = 0
+#keep dataset length to some fixed value. Takes the first n lines from a file
 f=open(sys.argv[1],'r')
 fo = open(sys.argv[2],'w')
 in_f=f.readlines()
